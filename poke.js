d5e15fd33ac7ae602421f2e41bb112f860c139ca
gaurav <93126046+gaurav1952@users.noreply.github.com>
2024-05-28 13:19:40 +0530

    Add files via upload
    add history page / modify the home pages

--- poke.js
+++ poke.js
@@ -1,6 +1,6 @@
 let divison = document.querySelector(".apidata");
 let userinputbe = document.querySelector("#userinput");
-
+let pokeimagesdivison  = document.querySelector(".pokeimagesdivision") 
 let available_pokemon = [
   "bulbasaur",
   "venusaur",
@@ -26,32 +26,87 @@ let available_pokemon = [
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#submit").addEventListener("click", () => {
     let uservalue = document.querySelector("#userinput").value;
+    console.log('uservalue: ', uservalue);
+    
     let url = `https://pokeapi.co/api/v2/pokemon/${uservalue}`;
     divison.textContent = "";
+
+
     if (uservalue != "") {
       fetch(url)
-        .then((res) => {
+        .then(res => {
           return res.json();
         })
-        .then((data) => {
+        .then(data => {
           console.log(data);
-
+          // On fetching , this section is to show case the data in index file
           let htmldata = `
        <h3 class = "pokemonheight"> Height of ${data.name} is ${data.height}</h3>
        <h3 class = "pokemonexe"> And their Base exprience is ${data.base_experience}</h3>
       `;
           divison.insertAdjacentHTML("afterbegin", htmldata);
+          imagesarray =data.sprites
+          console.log('imagesarray: ', imagesarray);
+
+          front_default = document.createElement("img")
+          front_default.classList.add("pokeimgs")
+          back_default = document.createElement("img")
+          back_default.classList.add("pokeimgs")
+          front_female = document.createElement("img")
+          front_female.classList.add("pokeimgs")
+          back_female = document.createElement("img")
+          back_female.classList.add("pokeimgs")
+
+         
+          front_default.src = imagesarray.front_default
+          front_default.alt = `${data.name}'s front default`
+          back_default.src = imagesarray.back_default
+          back_default.alt = `${data.name}'s back defalut`
+          front_female.src = imagesarray.front_female
+          front_female.alt = `${data.name}'s front female    `
+          back_female.src = imagesarray.back_female
+          back_female.alt = `${data.name}'s back female`
+
+
+
+          pokeimagesdivison.appendChild(front_default)
+          pokeimagesdivison.appendChild(back_default)
+          pokeimagesdivison.appendChild(front_female)
+          pokeimagesdivison.appendChild(back_female)
           // divison.appendChild(htmldata)
-          userinputbe.value = "";
-        });
+          uservalue.value = "";
+
+
+           
+        })
+        .catch(err=>{
+          console.log('Error: ', Error);
+          let errorhtml =`
+            <h3>${err}</h3>
+          `
+          divison.insertAdjacentHTML("afterbegin", errorhtml)
+          
+        })
+        
+        
     } else {
       let htmldata = `
-        <h3> Enter a pokemon name from first genration </h3>
+      
+        <h3>Enter a pokemon name from first genration!➕🌟⚡ </h3><span>!(*￣(￣　*)</span>
       `;
       divison.insertAdjacentHTML("afterbegin", htmldata);
+      pokeimagesdivison.innerHTML = ""
     }
   });
 });
+
+
+
+
+
+
+
+
 
 //todo make it async await
 // add click down page to show all the available pokemon
